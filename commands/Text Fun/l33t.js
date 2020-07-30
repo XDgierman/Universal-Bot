7be3cd350aac03df5e3e4ec5a0f1a3928b780250
XDgierman <xdgierman@interia.pl>
2020-07-30 13:40:25 +0200

    Update l33t.js

--- commands/Text Fun/l33t.js
+++ commands/Text Fun/l33t.js
@@ -20,7 +20,6 @@ function l33t(text){
     .replace(/hacker/gi, 'h4x0r')
 		.replace(/hack|hacks/gi, 'h4x')
     .replace(/Sorry|sorry/gi, 'sry')
-    .replace(/The|the/gi, 't3h')
     .replace(/and/gi, 'nd')
 		.replace(/Dude|dude/gi, 'd00d')
 		.replace(/Wait|wait/gi, 'w8')
@@ -28,7 +27,9 @@ function l33t(text){
     .replace(/[Aa]/g, '4')
     .replace(/[Ee]/g, '3')
     .replace(/[Oo]/g, '0')
-    .replace(/[Ii]/g, '1')
+    .replace(/[LlIi]/g, '1')
+		.replace(/[Tt]/g, '7')
+		.replace(/7h3/gi, 't3h')
     .replace(/[FU]/g, 'V')
     .replace(/[fu]/g, 'v')
 }
